661289f8a7e74d6228286e0bd82b7428c256c755
JuanJoseCanoFernandez <56116040+JuanJoseCanoFernandez@users.noreply.github.com>
2020-06-17 18:40:49 +0200

    Update config.py

--- Framework_Flask/aplicacion/config.py
+++ Framework_Flask/aplicacion/config.py
@@ -4,5 +4,5 @@ SECRET_KEY = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 PWD = os.path.abspath(os.curdir)
 
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://juanjo:juanjosecano7@juanjo.mysql.pythonanywhere-services.com/juanjo$AnalisisTwitter'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///{}/dbase.db'.format(PWD)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
